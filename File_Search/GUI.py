b2935a107cba585f65211b32f8f4bc97e885d60f
Guillaume SOURIS <gsouris@generali.fr>
2021-08-03 14:56:50 +0200

    Pylinter on gui.py

--- File_Search/GUI.py
+++ File_Search/GUI.py
@@ -1,7 +1,11 @@
+"""
+Graphical User Interface implemented with PySimpleGUI
+"""
+import os
+import sys
+import asyncio
 import PySimpleGUI as sg
 from File_Search.file_parser import findFilesInSet
-import os, sys
-import asyncio
 
 __SEARCHING__ = False
 __INTERRUPT__ = False
@@ -14,24 +18,28 @@ layout = [
     [sg.Exit()]
 ]
 
-def merge_nested_if(a, b):
-    if a:
-        if b:
-            return 2
-
 
 window = sg.Window('Py Simple Search', layout, finalize=True)
 
 async def wait_list(my_set):
-    await asyncio.wait([background(my_set), ui()])
+    """
+    builds task list for asynIO
+    """
+    await asyncio.wait([background(my_set), handle_ui()])
 
 def construct_interface(my_set):
+    """
+    prepares event_loop for interface handling
+    """
     loop = asyncio.get_event_loop()
     loop.run_until_complete(wait_list(my_set))
     loop.close()
 
 
-async def ui():
+async def handle_ui():
+    """
+    UI build and handling
+    """
     last_search = ''
     global __INTERRUPT__
     global __SEARCHING__
@@ -49,7 +57,7 @@ async def ui():
         elif event == '-RESULT-':
             file_clicked = values['-RESULT-'][0]
             if __SEARCHING__:
-                    __INTERRUPT__ = True
+                __INTERRUPT__ = True
             os.startfile(file_clicked)
         elif event == '__TIMEOUT__':
             pass
@@ -59,6 +67,9 @@ async def ui():
 
 
 async def background(my_set):
+    """
+    background task (search) and interuption handling
+    """
     search = ''
     while True:
         if window['-INPUT-'].get() != search:
@@ -67,9 +78,9 @@ async def background(my_set):
             lst.clear()
             window['-NB-'].update(0)
             i = 0
-            for r in findFilesInSet(my_set,search):
+            for item in findFilesInSet(my_set,search):
                 i += 1
-                lst.append(r)
+                lst.append(item)
                 global __SEARCHING__
                 global __INTERRUPT__
                 if __SEARCHING__ and __INTERRUPT__:
@@ -85,4 +96,3 @@ async def background(my_set):
             window['-RESULT-'].update(lst)
             window['-NB-'].update(len(lst))
         await asyncio.sleep(0.001)
-
